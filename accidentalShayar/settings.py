a33636dedd65c92e7260d579a5255cd8fb53b6f5
uttam77 <uttamjain7450@gmail.com>
2020-01-23 10:01:24 +0530

    Database changes

--- accidentalShayar/settings.py
+++ accidentalShayar/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'accidentalShayar.wsgi.application'
 #         'PORT': 5435
 #     }
 # }
-# DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
-DATABASES = {}
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+# DATABASES = {}
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
