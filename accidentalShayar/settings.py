6f4a80d2f4dc8b11c92eb40065a3c2cc556217e9
uttam77 <uttamjain7450@gmail.com>
2020-03-01 11:51:55 +0530

    New shayaris

--- accidentalShayar/settings.py
+++ accidentalShayar/settings.py
@@ -96,7 +96,6 @@ WSGI_APPLICATION = 'accidentalShayar.wsgi.application'
 #     }
 # }
 DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
